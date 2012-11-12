ca0e8e08592d7080e52566c4ed838d8abf2690ae
David Meads <dbmeads@hotmail.com>
2012-11-11 23:34:15 -0500

    Added some testing for Apply.util.isString

--- test/apply/0.0.1/specs/Apply.util.isString.js
+++ test/apply/0.0.1/specs/Apply.util.isString.js
@@ -1,4 +1,29 @@
 /*global Apply, describe, it, expect */
 describe('Apply.util.isString', function() {
     'use strict';
+
+    it('should not recognize an object as a string', function() {
+       expect(Apply.util.isString({})).toBe(false);
+    });
+
+    it('should not recognize a number as a string', function() {
+        expect(Apply.util.isString(0)).toBe(false);
+    });
+
+    it('should not recognize an array as a string', function() {
+        expect(Apply.util.isString([])).toBe(false);
+    });
+
+    it('should not recognize undefined as a string', function() {
+        expect(Apply.util.isString(undefined)).toBe(false);
+    });
+
+    it('should not recognize a function as a string', function() {
+        expect(Apply.util.isString(function() {})).toBe(false);
+    });
+
+    it('should not recognize a string as a string', function() {
+        expect(Apply.util.isString('')).toBe(true);
+    });
+
 });
