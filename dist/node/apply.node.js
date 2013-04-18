7350b7b302be78b4ee1ed82c47200dbf00b12ad3
David Meads <dbmeads@hotmail.com>
2013-04-18 12:59:05 -0400

    Changed the concat order for model so that it loads in the proper sequence.

--- dist/node/apply.node.js
+++ dist/node/apply.node.js
@@ -622,6 +622,93 @@ define('apply/events', ['apply/construct'], function (apply) {
 	return apply;
 });
 /*
+ * Module: apply/model
+ * Copyright 2012 David Meads
+ * Released under the MIT license
+ */
+define('apply/model', ['apply/events'], function (apply) {
+	'use strict';
+
+	var extend = apply.extend;
+
+	function inflate(object, mappings, parent) {
+		if (mappings) {
+			if (!object) {
+				object = {};
+			}
+			for (var key in mappings) {
+				if (object[key]) {
+					var obj = new mappings[key](object[key]);
+					if (parent) {
+						obj.parent = parent;
+					}
+					object[key] = obj;
+				}
+			}
+		}
+		return object;
+	}
+
+	function deflate(object, mappings) {
+		if (mappings) {
+			for (var key in mappings) {
+				if (object[key].deflate) {
+					object[key] = object[key].deflate();
+				}
+			}
+		}
+		return object;
+	}
+
+	function defaultAttributes(model) {
+		model.attributes = {};
+		model.set(model.
+	default);
+	}
+
+	apply.Model = apply.Events({
+		id: 'id',
+		urlRoot: '',
+		init: function (attributes) {
+			defaultAttributes(this);
+			this.set(attributes);
+		},
+		set: function (attributes) {
+			if (attributes && apply.isFunction(attributes.deflate)) {
+				attributes = attributes.deflate();
+			}
+			extend(this.attributes, inflate(attributes, this.mappings, this));
+			for (var key in attributes) {
+				this.trigger('change:' + key, this.attributes[key], this);
+			}
+			this.trigger('change', this.attributes[key], key, this);
+			return this;
+		},
+		get: function (key) {
+			return this.attributes[key];
+		},
+		getId: function () {
+			return this.attributes[this.id];
+		},
+		clear: function () {
+			var attributes = this.attributes;
+			defaultAttributes(this);
+			for (var key in attributes) {
+				this.trigger('change:' + key, undefined, this);
+				this.trigger('change', undefined, key, this);
+			}
+		},
+		deflate: function () {
+			return deflate(extend({}, this.attributes), this.mappings);
+		},
+		inflate: function (data) {
+			return this.set(data);
+		}
+	});
+
+	return apply;
+});
+/*
  * Module: apply/list
  * Copyright 2012 David Meads
  * Released under the MIT license
@@ -747,93 +834,6 @@ define('apply/logger', ['apply/construct'], function (apply) {
 	}).cascade('log', true);
 
 	apply.logger = Logger.instance();
-
-	return apply;
-});
-/*
- * Module: apply/model
- * Copyright 2012 David Meads
- * Released under the MIT license
- */
-define('apply/model', ['apply/events'], function (apply) {
-	'use strict';
-
-	var extend = apply.extend;
-
-	function inflate(object, mappings, parent) {
-		if (mappings) {
-			if (!object) {
-				object = {};
-			}
-			for (var key in mappings) {
-				if (object[key]) {
-					var obj = new mappings[key](object[key]);
-					if (parent) {
-						obj.parent = parent;
-					}
-					object[key] = obj;
-				}
-			}
-		}
-		return object;
-	}
-
-	function deflate(object, mappings) {
-		if (mappings) {
-			for (var key in mappings) {
-				if (object[key].deflate) {
-					object[key] = object[key].deflate();
-				}
-			}
-		}
-		return object;
-	}
-
-	function defaultAttributes(model) {
-		model.attributes = {};
-		model.set(model.
-	default);
-	}
-
-	apply.Model = apply.Events({
-		id: 'id',
-		urlRoot: '',
-		init: function (attributes) {
-			defaultAttributes(this);
-			this.set(attributes);
-		},
-		set: function (attributes) {
-			if (attributes && apply.isFunction(attributes.deflate)) {
-				attributes = attributes.deflate();
-			}
-			extend(this.attributes, inflate(attributes, this.mappings, this));
-			for (var key in attributes) {
-				this.trigger('change:' + key, this.attributes[key], this);
-			}
-			this.trigger('change', this.attributes[key], key, this);
-			return this;
-		},
-		get: function (key) {
-			return this.attributes[key];
-		},
-		getId: function () {
-			return this.attributes[this.id];
-		},
-		clear: function () {
-			var attributes = this.attributes;
-			defaultAttributes(this);
-			for (var key in attributes) {
-				this.trigger('change:' + key, undefined, this);
-				this.trigger('change', undefined, key, this);
-			}
-		},
-		deflate: function () {
-			return deflate(extend({}, this.attributes), this.mappings);
-		},
-		inflate: function (data) {
-			return this.set(data);
-		}
-	});
 
 	return apply;
 });
