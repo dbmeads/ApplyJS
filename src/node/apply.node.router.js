17834531264d27c02ce8cb6bb4bade054442cc30
David Meads <dbmeads@hotmail.com>
2013-04-22 23:20:11 -0400

    Fixed an issue with the node router.

--- src/node/apply.node.router.js
+++ src/node/apply.node.router.js
@@ -9,8 +9,8 @@ define('apply/node/router', ['apply'], function (apply) {
 	var invoke = apply.router.invoke;
 
 	apply.router.invoke = function (callback, args) {
-		if (apply.isPlainObject(callback) && apply.isDefined('0.method', args)) {
-			callback = callback[args[0].method];
+		if (apply.isPlainObject(callback) && apply.isDefined('1.method', args)) {
+			callback = callback[args[1].method];
 		}
 		return invoke.call(this, callback, args);
 	};
